c4a4620d9299818b072df462ee65185d8d7c372c
sgarcia <sgarcialaguna@googlemail.com>
2014-01-28 19:50:29 +0100

    added junit reports

--- grunt-qunit/Gruntfile.js
+++ grunt-qunit/Gruntfile.js
@@ -1,12 +1,20 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-		qunit: {
-			all: ['tests/*.html']
-		}
+        qunit: {
+            all: ['tests/*.html']
+        },
+
+        qunit_junit: {
+            options: {
+
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-qunit-junit')
+    grunt.registerTask('junit', ['qunit_junit', 'qunit']);
     grunt.registerTask('default', ['qunit'])
 }
