139ddc728e3fbb2b3f7859aa50ee52f3d8c95294
sgarcia <sgarcialaguna@googlemail.com>
2014-01-28 19:42:53 +0100

    fixed grunt config

--- grunt-qunit/Gruntfile.js
+++ grunt-qunit/Gruntfile.js
@@ -2,11 +2,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        taskName: {
-            qunit: {
-                all: ['tests/*.html']
-            }
-        }
+		qunit: {
+			all: ['tests/*.html']
+		}
     });
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
